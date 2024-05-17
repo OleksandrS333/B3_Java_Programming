769295a375c99db970d553734ccead9205cdf932
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-05-16 21:58:51 -0400

    method overloading, var-args, type promotion

---
+++ src/day26_methods/Painter.java
@@ -0,0 +1,23 @@
+package day26_methods;
+
+public class Painter {
+
+    public static void main(String[] args) {
+
+        day26_methods.Picture.draw();
+
+        day26_methods.Picture.draw("yellow");
+
+        day26_methods.Picture.draw("green", "red");
+
+       day26_methods.Picture.draw(4);
+
+        day26_methods.Picture.draw(2, "pink");
+
+
+        day26_methods.Picture.draw("pink", 2);
+
+
+    }
+
+}
